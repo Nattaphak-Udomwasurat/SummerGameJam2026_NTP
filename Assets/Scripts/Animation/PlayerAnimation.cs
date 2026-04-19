9bcd8fb850468ec86c28da2f0c3eaf3faca82b7f
blacOOil <111561598+blacOOil@users.noreply.github.com>
2026-04-19 23:14:07 +0700

    PuPlayer Animation

---
+++ Assets/Scripts/Animation/PlayerAnimation.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    public List<Sprite> AnimationSprite;  // [0] = idle, [1] = walk
+    public SpriteRenderer PlayerSprite;
+    public float delay = 0.3f;
+    private Vector3 lastPosition;
+    private float timer = 0f;
+
+    void Start()
+    {
+        lastPosition = transform.position;
+    }
+
+    void Update()
+    {
+        bool isMoving = transform.position != lastPosition;
+
+        if (isMoving)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= delay)
+            {
+                PlayerSprite.sprite = AnimationSprite[1];
+            }
+
+            // flip based on horizontal direction
+            if (transform.position.x < lastPosition.x)
+            {
+                PlayerSprite.flipX = false;   // moving left
+            }
+            else if (transform.position.x > lastPosition.x)
+            {
+                PlayerSprite.flipX = true;  // moving right
+            }
+        }
+        else
+        {
+            timer = 0f;
+            PlayerSprite.sprite = AnimationSprite[0];
+        }
+
+        lastPosition = transform.position;
+    }
+}
+    
